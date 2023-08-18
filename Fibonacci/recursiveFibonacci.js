7b595287835854b8f2a76835cba60e00929ee437
Mustafa Topal <mustafatopal@MacBook-Pro-de-Mustafa.local>
2023-08-18 22:29:10 +0200

    rename javascript files to match their content

---
+++ Fibonacci/recursiveFibonacci.js
@@ -0,0 +1,15 @@
+function recursiveFibo(n) {
+    if (n === 0) {
+      return 0;
+    }
+    if (n === 1) {
+      return 1;
+    }
+  
+    return recursiveFibo(n - 1) + recursiveFibo(n - 2);
+  }
+  const numberOfTerms = 10;
+  for (let i = 0; i < numberOfTerms; i++) {
+    const result = recursiveFibo(i);
+    console.log(result);
+  }
